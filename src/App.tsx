8c40b83cb4905e50eb90d6071aac403b2c65a6cb
Mohib <mbmohib@gmail.com>
2021-10-08 11:48:37 +0600

    organize under folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
-import { theme } from './theme'
-import { store } from './store'
+import { theme } from './styles/theme'
+import { store } from './store/store'
 import { Provider } from 'react-redux'
 
 export default function App() {
