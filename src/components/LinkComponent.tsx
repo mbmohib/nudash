de1a5895480060e3343cd049fd6b19be72ed365f
Mohib <mbmohib@gmail.com>
2021-11-17 12:40:01 +0600

    add link component

---
+++ src/components/LinkComponent.tsx
@@ -0,0 +1,53 @@
+import { Box, Grid, Input, Link } from '@chakra-ui/react';
+import { useState } from 'react';
+
+import { ComponentAction } from '.';
+import { useDispatch, useSection } from '../hooks';
+import { handleFieldData } from '../store/sectionSlice';
+import { FieldProps } from '../types';
+
+export default function ButtonComponent({ field }: FieldProps) {
+  const { sectionId, rowId, columnId } = useSection();
+  const dispatch = useDispatch();
+  const [label, setLabel] = useState<string>('');
+  const [value, setValue] = useState<string>('');
+
+  const handleSaveData = () => {
+    dispatch(
+      handleFieldData({
+        dropZoneId: field.id,
+        sectionId,
+        rowId,
+        columnId,
+        data: {
+          label,
+          value,
+        },
+      }),
+    );
+  };
+
+  return (
+    <>
+      {field.data ? (
+        <Link to={field.data.value}>{field.data.label}</Link>
+      ) : (
+        <Box>
+          <Grid gridTemplateColumns="1fr 3fr" gap="1" width="100%">
+            <Input
+              onChange={event => setLabel(event.target.value)}
+              type="text"
+              placeholder="label"
+            />
+            <Input
+              type="text"
+              placeholder="Link"
+              onChange={event => setValue(event.target.value)}
+            />
+          </Grid>
+          <ComponentAction handleSave={handleSaveData} />
+        </Box>
+      )}
+    </>
+  );
+}
