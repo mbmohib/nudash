db5f9355444145d7ec8c8b1a012a7a45bc4e53cb
Mohib <mbmohib@gmail.com>
2021-12-23 11:06:39 +0600

    addd test for all rotues

--- src/__test__/MetaData.test.tsx
+++ src/__test__/MetaData.test.tsx
@@ -1,31 +1,24 @@
 import { screen, waitFor } from '@testing-library/react';
+import MatchMediaMock from 'jest-matchmedia-mock';
 
 import { siteData } from '../mocks/db';
 import { server } from '../mocks/server';
 import { MetaData } from '../screens';
 import { renderWithRouter as render } from '../utils/test';
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: (query: unknown) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  }),
-});
+let matchMedia: MatchMediaMock;
 
 beforeAll(() => {
+  matchMedia = new MatchMediaMock();
   server.listen({ onUnhandledRequest: 'error' });
 });
 afterAll(() => {
   server.close();
 });
-afterEach(() => server.resetHandlers());
+afterEach(() => {
+  server.resetHandlers();
+  matchMedia.clear();
+});
 
 test('show site data in site info update form', async () => {
   render(<MetaData />);
--- src/__test__/Routes.test.tsx
+++ src/__test__/Routes.test.tsx
@@ -1,7 +1,28 @@
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import MatchMediaMock from 'jest-matchmedia-mock';
+import mockConsole from 'jest-mock-console';
 
+import { server } from '../mocks/server';
 import Routes from '../routes';
 import { renderWithRouter as render, userEvent } from '../utils/test';
+
+let restoreConsole: { (): void; (): void };
+let matchMedia: MatchMediaMock;
+
+beforeAll(() => {
+  restoreConsole = mockConsole();
+  server.listen({ onUnhandledRequest: 'error' });
+  matchMedia = new MatchMediaMock();
+});
+
+afterEach(() => {
+  server.resetHandlers();
+  matchMedia.clear();
+});
+afterAll(() => {
+  server.close();
+  restoreConsole();
+});
 
 test('render dashboard analytics page', () => {
   render(<Routes />);
@@ -12,19 +33,44 @@ test('render dashboard analytics page', () => {
 });
 
 test('render site page on clicking site menu', async () => {
-  const { getByRole, getByTestId, debug } = render(<Routes />);
+  render(<Routes />);
 
-  const siteLink = getByTestId(/site-link/i);
-  // debug(siteLink);
-  userEvent.click(siteLink);
+  userEvent.click(screen.getByTestId('site-link'));
 
-  // const heading = getByRole('heading', { name: /page heading/i });
-  // expect(heading).toHaveTextContent(/site/i);
+  await waitFor(() => {
+    expect(
+      screen.getByRole('heading', { name: /page heading/i }),
+    ).toHaveTextContent(/meta data/i);
+  });
+});
+
+test('render page builder on clicking page menu', async () => {
+  render(<Routes />);
+
+  userEvent.click(screen.getByTestId('page-link'));
+
+  await waitFor(() => {
+    expect(
+      screen.getByRole('heading', { name: /page heading/i }),
+    ).toHaveTextContent(/home/i);
+  });
+});
+
+test('render images page on clicking gallery menu', async () => {
+  render(<Routes />);
+
+  userEvent.click(screen.getByTestId('gallery-link'));
+
+  await waitFor(() => {
+    expect(
+      screen.getByRole('heading', { name: /page heading/i }),
+    ).toHaveTextContent(/images/i);
+  });
 });
 
 test('render 404 page', () => {
-  const { getByRole } = render(<Routes />, { route: 'no-match-page' });
+  render(<Routes />, { route: 'no-match-page' });
 
-  const heading = getByRole(/heading/i);
+  const heading = screen.getByRole(/heading/i);
   expect(heading).toHaveTextContent(/404/i);
 });
