1e5d558399eef5c0ca25d1b846827398f9e7ec48
Mohib <mbmohib@gmail.com>
2022-01-19 17:05:00 +0600

    integrate msw js with cypress

--- cypress/e2e/login.test.ts
+++ cypress/e2e/login.test.ts
@@ -1,10 +1,28 @@
 describe('login', () => {
-  it('should login user', () => {
+  it('should login an existing user', () => {
     cy.visit('/');
     cy.findByLabelText(/email/i).type('example@gmail.com');
     cy.findByLabelText(/password/i).type('123456');
     cy.findByRole('button', { name: /login/i }).click();
     cy.findByText(/dashboard/i);
-    // cy.url().should('eq', 'http://localhost:3000/dashboard/analytics');
+  });
+
+  it(`should show an error message if there's an error on login`, () => {
+    cy.visit('/login');
+    cy.window().then(window => {
+      const { worker, rest } = window.msw;
+
+      worker.use(
+        rest.post('http://localhost:4000/login', (req, res, ctx) => {
+          return res(ctx.status(401), ctx.json({}));
+        }),
+      );
+
+      cy.findByLabelText(/email/i).type('example@gmail.com');
+      cy.findByLabelText(/password/i).type('123456');
+      cy.findByRole('button', { name: /login/i }).click();
+
+      cy.findByText(/try again/i);
+    });
   });
 });
