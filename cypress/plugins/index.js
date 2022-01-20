15535ca98e215b01d027a1d7d8cfcbaed579b789
Mohib <mbmohib@gmail.com>
2022-01-20 11:14:53 +0600

    convert cypress file to ts

--- cypress/plugins/index.js
+++
@@ -1,7 +0,0 @@
-/// <reference types="cypress" />
-const injectDevServer = require('@cypress/react/plugins/react-scripts');
-
-module.exports = (on, config) => {
-  injectDevServer(on, config);
-  return config;
-};
